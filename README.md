9d11812f21013708ae26bf81eb159b3213b7a68e
xbze3 <ezranminty@gmail.com>
2025-10-12 23:22:15 -0400

    Initial structural commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FinanceGY
+
+Unofficial Python library for accessing Gaskii financial data.
+
+```python
+import financegy
+
+```
---
+++ financegy/config.py
@@ -0,0 +1,7 @@
+from financegy import __version__
+
+BASE_URL = "https://guyanastockexchangeinc.com/"
+HEADERS = {
+    "User-Agent": f"FinanceGY/{__version__} (https://github.com/xbze3/financegy)"
+}
+REQUEST_TIMEOUT = 10
